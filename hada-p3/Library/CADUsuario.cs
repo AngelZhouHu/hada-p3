b4c6920274ef17de1575504aff4826d35d2e5537
Angel Zhou Hu <zh2@alu.ua.es>
2019-03-15 14:53:05 +0100

    02300496J Cambio a los nombres de las clases y creación del esquema de las clases

---
+++ hada-p3/Library/CADUsuario.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class CADUsuario
+    {
+        private const string cadena = "0";
+
+        public CADUsuario()
+        {
+
+        }
+        public bool createUsuario(ENUsuario en)
+        {
+            return false;
+        }
+        public bool readUsuario(ENUsuario en)
+        {
+            return false;
+        }
+        public bool readFirstUsuario(ENUsuario en)
+        {
+            return false;
+        }
+        public bool readNextUsuario(ENUsuario en)
+        {
+            return false;
+        }
+        public bool readPrevUsuario(ENUsuario en)
+        {
+            return false;
+        }
+        public bool updateUsuario(ENUsuario en)
+        {
+            return false;
+        }
+        public bool deleteUsuario(ENUsuario en)
+        {
+            return false;
+        }
+
+    }
+}
---
+++ hada-p3/Library/ENUsuario.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class ENUsuario
+    {
+        private string nif;
+        private string nombre;
+        private int edad;
+
+        public ENUsuario()
+        {
+
+        }
+        public ENUsuario(string nif, string nombre, string edad)
+        {
+
+        }
+        public bool createUsuario()
+        {
+            return false;
+        }
+        public bool readUsuario()
+        {
+            return false;
+        }
+        public bool readFirstUsuario()
+        {
+            return false;
+        }
+        public bool readNextUsuario()
+        {
+            return false;
+        }
+        public bool readPrevUsuario()
+        {
+            return false;
+        }
+        public bool updateUsuario()
+        {
+            return false;
+        }
+        public bool deleteUsuario()
+        {
+            return false;
+        }
+    }
+}
