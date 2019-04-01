d73fdc4ce0828bd7a3c7498a859dd882515785b7
Angel Zhou Hu <zh2@alu.ua.es>
2019-04-01 16:56:58 +0200

    02300496J Actualizada la base de datos que antes no iba

--- hada-p3/usuWeb/App_Data/dbo.Usuarios.sql
+++ hada-p3/usuWeb/App_Data/dbo.Usuarios.sql
@@ -1,11 +1,4 @@
 ﻿CREATE TABLE [dbo].[Usuarios]
 (
-
-	[id] INT IDENTITY (1, 1) NOT NULL, 
-    [nombre] NVARCHAR(32) NOT NULL, 
-    [nif] NCHAR(16) NOT NULL, 
-    [edad] INT NOT NULL,
-	PRIMARY KEY CLUSTERED ([id] ASC),
-	UNIQUE NONCLUSTERED ([nif] ASC)
+	[Id] INT NOT NULL PRIMARY KEY
 )
-
